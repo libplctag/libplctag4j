b213e7e40c076b7fac6f1397f2e481f7ca2cbef3
Kyle Hayes <kyle.hayes@gmail.com>
2020-09-05 12:08:18 -0700

    Added tests to check callback state.

--- src/test/java/libplctag/java/TagTest.java
+++ src/test/java/libplctag/java/TagTest.java
@@ -40,7 +40,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TagTest {
-	static final int TIMEOUT = 5000;
+    static final int TIMEOUT = 5000;
+    static int lastState = -1;
 	
     @Test public void testTag() {
         // set the debug level.
@@ -56,26 +57,32 @@ public class TagTest {
                 switch(event) {
                     case Tag.PLCTAG_EVENT_ABORTED:
                         eventMsg = "operation aborted";
+                        lastState = Tag.PLCTAG_EVENT_ABORTED;
                         break;
 
                     case Tag.PLCTAG_EVENT_DESTROYED:
                         eventMsg = "being destroyed";
+                        lastState = Tag.PLCTAG_EVENT_DESTROYED;
                         break;
 
                     case Tag.PLCTAG_EVENT_READ_COMPLETED:
                         eventMsg = "read completed";
+                        lastState = Tag.PLCTAG_EVENT_READ_COMPLETED;
                         break;
 
                     case Tag.PLCTAG_EVENT_READ_STARTED:
                         eventMsg = "read started";
+                        lastState = Tag.PLCTAG_EVENT_READ_STARTED;
                         break;
 
                     case Tag.PLCTAG_EVENT_WRITE_COMPLETED:
                         eventMsg = "write completed";
+                        lastState = Tag.PLCTAG_EVENT_WRITE_COMPLETED;
                         break;
 
                     case Tag.PLCTAG_EVENT_WRITE_STARTED:
                         eventMsg = "write started";
+                        lastState = Tag.PLCTAG_EVENT_WRITE_STARTED;
                         break;
 
                     default:
@@ -97,12 +104,21 @@ public class TagTest {
 
         assertEquals("Unable to register logging callback!", Tag.registerLoggerCallback(loggingCallback), Tag.PLCTAG_STATUS_OK);
 
+        // get the tag value.
+        assertEquals("Unable to read debug tag!", tag.read(TIMEOUT), Tag.PLCTAG_STATUS_OK);
+
+        // check the last state from the callback. 
+        assertEquals("Last state should be PLCTAG_EVENT_READ_COMPLETED!", lastState, Tag.PLCTAG_EVENT_READ_COMPLETED);
+
         // write the DEBUG_DETAIL log value.
         assertEquals("Unable to update debug tag!", tag.setInt32(0, Tag.PLCTAG_DEBUG_INFO), Tag.PLCTAG_STATUS_OK);
         
         // write the tag.
         assertEquals("Unable to write debug tag!", tag.write(TIMEOUT), Tag.PLCTAG_STATUS_OK);
         
+        // check the last state from the callback. 
+        assertEquals("Last state should be PLCTAG_EVENT_WRITE_COMPLETED!", lastState, Tag.PLCTAG_EVENT_WRITE_COMPLETED);
+
         // attempt read the DEBUG log value.
         assertEquals("Debug value not set!", Tag.getLibraryAttribute("debug", -1), Tag.PLCTAG_DEBUG_INFO);
 
